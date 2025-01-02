223ee87775476e84b017d533c6e4f3e05003a05e
rameesvk551 <rameesvk551@gmail.com>
2025-01-02 10:53:52 +0530

    Initial commit with project files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  const theme='dark'
+  return (
+   <main className={theme}>
+    <div className="w-full min-h-screen relative bg-white dark:bg-[#020b19]"></div>
+   </main>
+  
+  );
+}
+
+export default App;
